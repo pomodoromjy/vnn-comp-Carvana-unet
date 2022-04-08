558fb3fed692712c4a15c4cba35c1ed6eb3f1ae9
pomodoromjy <pomodoromjy@gmail.com>
2022-04-08 16:35:57 +0800

    add two eps

--- src/generate_specs.py
+++ src/generate_specs.py
@@ -17,53 +17,54 @@ def img_preprocess(pil_img):
     img_ndarray = np.asarray(pil_img) / 255
     return img_ndarray
 
-def write_vnn_spec(img_pre, imagename, eps, dir_path, prefix="spec", data_lb=0, data_ub=1, n_class=1, mean=0.0, std=1.0, negate_spec=False,csv=''):
-    x = Image.open(img_pre + imagename)
-    x = np.array(x) / 255
-    x_lb = np.clip(x - eps, data_lb, data_ub)
-    x_lb = ((x_lb-mean)/std).reshape(-1)
-    x_ub = np.clip(x + eps, data_lb, data_ub)
-    x_ub = ((x_ub - mean) / std).reshape(-1)
+def write_vnn_spec(img_pre, imagename, epslion, dir_path, prefix="spec", data_lb=0, data_ub=1, n_class=1, mean=0.0, std=1.0, negate_spec=False,csv=''):
+    for eps in epslion:
+        x = Image.open(img_pre + imagename)
+        x = np.array(x) / 255
+        x_lb = np.clip(x - eps, data_lb, data_ub)
+        x_lb = ((x_lb-mean)/std).reshape(-1)
+        x_ub = np.clip(x + eps, data_lb, data_ub)
+        x_ub = ((x_ub - mean) / std).reshape(-1)
 
-    if not os.path.exists(dir_path):
-        os.mkdir(dir_path)
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
 
-    if np.all(mean==0.) and np.all(std==1.):
-        spec_name = f"{prefix}_idx_{imagename}_eps_{eps:.5f}.vnnlib"
-    else:
-        existing_specs = os.listdir(dir_path)
-        competing_norm_ids = [int(re.match(f"{prefix}_idx_{imagename}_eps_{eps:.5f}_n([0-9]+).vnnlib",spec).group(1)) for spec in existing_specs if spec.startswith(f"{prefix}_idx_{index}_eps_{eps:.5f}_n")]
-        norm_id = 1 if len(competing_norm_ids) == 0 else max(competing_norm_ids)+1
-        spec_name = f"{prefix}_idx_{imagename}_eps_{eps:.5f}_n{norm_id}.vnnlib"
-
-
-    spec_path = os.path.join(dir_path, spec_name)
-
-    with open(spec_path, "w") as f:
-        f.write(f"; Spec for sample id {imagename} and epsilon {eps:.5f}\n")
-
-        f.write(f"\n; Definition of input variables\n")
-        for i in range(len(x_ub)):
-            f.write(f"(declare-const X_{i} Real)\n")
-
-        f.write(f"\n; Definition of output variables\n")
-        for i in range(n_class):
-            f.write(f"(declare-const Y_{i} Real)\n")
-
-        f.write(f"\n; Definition of input constraints\n")
-        for i in range(len(x_ub)):
-            f.write(f"(assert (<= X_{i} {x_ub[i]:.8f}))\n")
-            f.write(f"(assert (>= X_{i} {x_lb[i]:.8f}))\n")
-
-        f.write(f"\n; Definition of output constraints\n")
-        if negate_spec:
-            for i in range(n_class-1):
-                f.write(f"(assert (<= Y_1 1166))\n")
+        if np.all(mean==0.) and np.all(std==1.):
+            spec_name = f"{prefix}_idx_{imagename}_eps_{eps:.5f}.vnnlib"
         else:
-            f.write(f"(assert (or\n")
+            existing_specs = os.listdir(dir_path)
+            competing_norm_ids = [int(re.match(f"{prefix}_idx_{imagename}_eps_{eps:.5f}_n([0-9]+).vnnlib",spec).group(1)) for spec in existing_specs if spec.startswith(f"{prefix}_idx_{index}_eps_{eps:.5f}_n")]
+            norm_id = 1 if len(competing_norm_ids) == 0 else max(competing_norm_ids)+1
+            spec_name = f"{prefix}_idx_{imagename}_eps_{eps:.5f}_n{norm_id}.vnnlib"
+
+
+        spec_path = os.path.join(dir_path, spec_name)
+
+        with open(spec_path, "w") as f:
+            f.write(f"; Spec for sample id {imagename} and epsilon {eps:.5f}\n")
+
+            f.write(f"\n; Definition of input variables\n")
+            for i in range(len(x_ub)):
+                f.write(f"(declare-const X_{i} Real)\n")
+
+            f.write(f"\n; Definition of output variables\n")
             for i in range(n_class):
-                f.write(f"\t(and (>= Y_1 1166))\n")
-            f.write(f"))\n")
+                f.write(f"(declare-const Y_{i} Real)\n")
+
+            f.write(f"\n; Definition of input constraints\n")
+            for i in range(len(x_ub)):
+                f.write(f"(assert (<= X_{i} {x_ub[i]:.8f}))\n")
+                f.write(f"(assert (>= X_{i} {x_lb[i]:.8f}))\n")
+
+            f.write(f"\n; Definition of output constraints\n")
+            if negate_spec:
+                for i in range(n_class-1):
+                    f.write(f"(assert (<= Y_1 1166))\n")
+            else:
+                f.write(f"(assert (or\n")
+                for i in range(n_class):
+                    f.write(f"\t(and (>= Y_1 1166))\n")
+                f.write(f"))\n")
     csv = csv
     if not os.path.exists(csv):
         os.system(r"touch {}".format(csv))
@@ -81,7 +82,7 @@ def write_vnn_spec(img_pre, imagename, eps, dir_path, prefix="spec", data_lb=0, 
 def main():
     parser = argparse.ArgumentParser(description='VNN spec generator',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--epsilon', type=float, default=0.012, help='The epsilon for L_infinity perturbation')
+    parser.add_argument('--epsilon', type=float, default=[0.012,0.015], help='The epsilon for L_infinity perturbation')
     parser.add_argument('--mean', nargs='+', type=float, default=0.0, help='the mean used to normalize the data with')
     parser.add_argument('--std', nargs='+', type=float, default=1.0,
                         help='the standard deviation used to normalize the data with')
